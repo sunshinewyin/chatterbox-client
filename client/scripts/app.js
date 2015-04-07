5a9a055dc3d490d8b82e7853cbbff071ba974021
sunshinewyin <sunshine.w.yin@gmail.com>
2015-04-06 17:26:31 -0700

    created App class added methods:  -init, fetch, send, addMessage, clearMessages, addRoom   addFriend// addFriend still in progress

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,80 @@
 // YOUR CODE HERE:
+//
+//
 
+var App = function() {
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
+  this.rooms = {};
+  this.friends = {};
+};
+
+App.prototype.init = function() {
+
+};
+
+App.prototype.send = function(message) {
+  $.ajax({
+    // always use this url
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+App.prototype.fetch = function () {
+  var context = this;
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    success: function(dataFromServer){
+      for(var i = 0; i < dataFromServer.results.length; ++i) {
+        context.addMessage(dataFromServer.results[i]);
+      }
+    }
+  });
+};
+
+App.prototype.clearMessages = function() {
+  $('#chats').children().remove();
+};
+
+App.prototype.addMessage = function (messageData) {
+  var text = messageData.text;
+  var createdAt = messageData.createdAt;
+  var $messageBlock = $("<div class='message'></div>");
+  var $message = $("<p>" + text + "</p>");
+  var $createdAt = $("<p>" + createdAt + "</p>");
+
+  $createdAt.appendTo($messageBlock);
+  $message.appendTo($messageBlock);
+  $('#chats').prepend($messageBlock);
+};
+
+App.prototype.addRoom = function(roomName) {
+  if(this.rooms.hasOwnProperty(roomName)) {
+    return;
+  }
+
+  this.rooms[roomName] = roomName;
+  var $roomName = $("<option value="+roomName+">"+roomName+"</option>");
+  $roomName.appendTo('#roomSelect');
+};
+
+App.prototype.addFriend = function(friendName) {
+  if(this.friends.hasOwnProperty(friendName)) {
+    return;
+  }
+
+  this.friends[friendName] = friendName;
+}
+
+
+var app = new App();
